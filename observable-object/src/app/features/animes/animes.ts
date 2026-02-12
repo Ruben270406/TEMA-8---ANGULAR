e35f352388ef64f0b59c43b4eda50fc40967f93f
Ruben <rvidabravo@gmail.com>
2026-02-12 11:01:06 +0100

    Eliminar datos

--- observable-object/src/app/features/animes/animes.ts
+++ observable-object/src/app/features/animes/animes.ts
@@ -11,4 +11,17 @@ import { AnimeApi } from './services/anime-api';
 export class Animes {
   protected readonly animesApi: AnimeApi = inject(AnimeApi);
 
+  protected deleteAnimeById(idAnime: number) {
+    if(!confirm(`¿Seguro que quieres eliminar el anime?`)) return;
+this.animesApi.deleteAnimeById(idAnime).subscribe({
+  next: () => {
+    this.animesApi.getAllAnimes.reload();
+    console.log(`Anime con id ${idAnime} eliminado correctamente`);
+},
+  error: (error) => {
+  console.error(`Error al eliminar el anime con id ${idAnime}:`, error);
+} 
+}); 
+}
+  
 }
--- observable-object/src/app/features/animes/services/anime-api.ts
+++ observable-object/src/app/features/animes/services/anime-api.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Anime } from '../models/anime';
-import { httpResource } from '@angular/common/http';
+import { HttpClient, httpResource } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -11,5 +12,8 @@ export class AnimeApi {
 public readonly getAllAnimes = httpResource<Anime[]>(() => ({ url: this.url
 }));
 
-
+  private http: HttpClient = inject(HttpClient);
+  public readonly deleteAnimeById = (idAnime: number): Observable<void> => {
+    return this.http.delete<void>(`${this.url}/${idAnime}`);
+  }
 }
